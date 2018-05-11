d96f5175342916c0cf87998622ad1b4d9f4a3cac
Paul Laux <lauxpaul@protonmail.com>
2018-05-11 14:12:34 +0300

    added documentation for ComplianceStore

--- contracts/token/ERC20/ComplianceStore.sol
+++ contracts/token/ERC20/ComplianceStore.sol
@@ -6,27 +6,34 @@ import "./MultihashStore.sol";
 /**
  * @title ComplianceStore
  * @dev enables to store optional Compliance data
- * @dev supports one multihash per address
+ * supports one multihash per address
+ * more information about multihash can be found on https://github.com/multiformats/multihash
+ * Important hash functions:
+ * Name, hashFunction, size (bytes)
+ * sha1, 0x11, 20
+ * sha2-256, 0x12, 32
+ * sha3-256, 0x16, 32
+ * Full list can be found on https://github.com/multiformats/multihash/blob/master/hashtable.csv
  */
 contract ComplianceStore is MultihashStore {
   event SetHash(address indexed from, uint8 hashFunction, uint8 size, bytes32 hash, uint256 timestamp);
 
   struct Multihash {
-    uint8 hashFunction;
-    uint8 size;
-    bytes32 hash;
-    uint256 timestamp;
+    uint8 hashFunction; // id of the hash function
+    uint8 size;         // size in bytes
+    bytes32 hash;       // the digest
+    uint256 timestamp;  // when modified last time
   }
 
   mapping( address => Multihash ) store;
 
 
   /** @dev add a multihash into the store
-      * @param _hashFunction Width of the rectangle.
-      * @param _size Height of the rectangle.
-      * @param _hash Height of the rectangle.
-      * @return true on success
-      */
+  * @param _hashFunction id of the function
+  * @param _size lenght of digest
+  * @param _hash the digest
+  * @return true on success
+  */
   function setHash(
     uint8 _hashFunction,
     uint8 _size,
