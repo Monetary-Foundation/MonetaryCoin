7bf502db8d7b9be07b2ebf382d8e666985ea8f63
Paul Laux <lauxpaul@protonmail.com>
2018-05-11 12:59:56 +0300

    cosmetic fix for getM5Reward

--- contracts/token/ERC20/MinableM5Token.sol
+++ contracts/token/ERC20/MinableM5Token.sol
@@ -132,7 +132,7 @@ contract MinableM5Token is GDPOraclizedToken {
     // method signeture for target contract
     bytes32 signature = keccak256("getM5Reward(address)");
     // size of calldata for getM5Reward function: 4 for signeture and 32 for one variable (address)
-    uint32 calldataSize = 4 + 32;
+    uint32 inputSize = 4 + 32;
     // variable to check delegatecall result (success or failure)
     uint8 callResult;
     // result from target.getM5Reward()
@@ -140,12 +140,12 @@ contract MinableM5Token is GDPOraclizedToken {
     
     assembly { // solium-disable-line
         // return _dest.delegatecall(msg.data)
-        mstore(0, signature) // 4 bytes of method signature
-        mstore(4, _miner)    // 20 bytes of address
+        mstore(0x0, signature) // 4 bytes of method signature
+        mstore(0x4, _miner)    // 20 bytes of address
         // delegatecall(g, a, in, insize, out, outsize)	- call contract at address a with input mem[in..(in+insize))
         // providing g gas and v wei and output area mem[out..(out+outsize)) returning 0 on error (eg. out of gas) and 1 on success
         // keep caller and callvalue
-        callResult := delegatecall(sub(gas, 10000), target, 0x0, calldataSize, 0, returnSize)
+        callResult := delegatecall(sub(gas, 10000), target, 0x0, inputSize, 0x0, returnSize)
         switch callResult 
         case 0 
           { revert(0,0) } 
